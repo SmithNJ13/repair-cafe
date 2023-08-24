e9c4ff49781c1588be5682da2361b4058cdd18eb
raviasaini <ravia.saini81@outlook.com>
2023-08-24 12:30:05 +0100

    modified a few syntax

--- static/js/newJob.js
+++ static/js/newJob.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const jobForm = document.getElementById("jobForm");
-    const jobPostedMessage = document.getElementById("jobPostedMessage");
+    const jobForm = document.getElementById("jobForm")
+    const jobPostedMessage = document.getElementById("jobPostedMessage")
     
     jobForm.addEventListener("submit", async (e) => {
-        e.preventDefault();
+        e.preventDefault()
         const formData = new FormData(jobForm);
-        const data = {};
+        const data = {}
         
         formData.forEach((value, key) => {
-            data[key] = value;
-        });
+            data[key] = value
+        })
         
         try {
             const response = await fetch("http://localhost:5000/jobs", {
@@ -19,20 +19,20 @@ document.addEventListener("DOMContentLoaded", async () => {
                     "Accept": "application/json"
                 },
                 body: JSON.stringify(data),
-            });
+            })
             
             if (response.status === 201) {
                 jobPostedMessage.style.display = "block";
-                console.log("Repair job posted successfully!");
+                console.log("Repair job posted successfully!")
                 jobForm.reset();
             } else if (response.status === 400) {
-                const errorData = await response.json();
-                console.error("Error posting repair job:", errorData.error);
+                const errorData = await response.json()
+                console.error("Error posting repair job:", errorData.error)
             } else {
-                console.error("Error posting repair job:", response.status, response.statusText);
+                console.error("Error posting repair job:", response.status, response.statusText)
             }
         } catch (error) {
-            console.error("Error posting repair job:", error);
+            console.error("Error posting repair job:", error)
         }
-    });
-});
+    })
+})
